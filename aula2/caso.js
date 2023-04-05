4edbaa4cfbc8aae921c3307be76d00320885dfe7
Renato Moura Cvalcante <boniex01@gmail.com>
2023-04-05 13:57:18 -0300

    caso js

---
+++ aula2/caso.js
@@ -0,0 +1,20 @@
+let nota = 4
+
+switch (nota) {
+    case 10:
+        console.log('Aprovado com nota máxima!!!')
+        break
+    case 9:
+    case 8:
+    case 7:
+        console.log('Aprovado por media!!!')
+        break
+    case 6:
+    case 5:
+    case 4:
+    case 3:
+    case 2:
+    case 1:
+        console.log('Reprovado!!!')
+
+}
---
+++ aula2/operadorTern.js
@@ -0,0 +1,5 @@
+var nota = 10
+
+let media = nota >= 7 ? 'Aprovado': 'Reprovado'
+
+console.log(media)
